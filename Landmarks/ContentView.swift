2c957ed1b96ccf2936d17cec11cd72f7930c9fbc
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2020-03-27 19:12:23 -0400

    creating and combining views pt 2/7

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Text changed.")
+        Text("Turtle Rock")
+            .font(.title)
+            
     }
 }
 
