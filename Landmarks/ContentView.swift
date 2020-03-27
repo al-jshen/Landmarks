e6cdc1b1fde7f6a8aadd1df97ab6768b3ab0ed5a
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2020-03-27 18:56:33 -0400

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jeff Shen on 2020-03-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
