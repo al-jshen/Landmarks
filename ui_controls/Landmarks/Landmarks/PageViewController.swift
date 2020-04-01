02484817148310b4328c2eff0c352006fe7df79a
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2020-03-31 21:38:46 -0400

    nvm skipping uikit lesson

---
+++ ui_controls/Landmarks/Landmarks/PageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Jeff Shen on 2020-03-31.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController: UIViewControllerRepresentable {
+    var controllers: [UIViewController]
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal
+        )
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [controllers[0]], direction: .forward, animated: true
+        )
+    }
+}
+
+
